957bf8d47404b6110a201aa278ae20b1ef81aeed
Tiffany Sawyer <stephen.sawyer64@gmail.com>
2016-04-13 23:52:39 +0100

    Started work on putting dimensional transformations into Clojure, a Lisp dialect that will communicate with the Java program

---
+++ clojureStuff/coordinatehandler/test/coordinatehandler/core_test.clj
@@ -0,0 +1,7 @@
+(ns coordinatehandler.core-test
+  (:require [clojure.test :refer :all]
+            [coordinatehandler.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
