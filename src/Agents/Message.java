96503059cc3e03eded3d7557418b972e0c3b2f72
Lu9us <n3148087@live.tees.ac.uk>
2016-04-13 20:42:35 +0100

    Correct Package
    correcting the package line in the sauces

--- src/Agents/Message.java
+++ src/Agents/Message.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package masproject.Agents;
+package Agents;
 
 import java.io.Serializable;
 
